4db39a20208a8ec29f367e4eacaa20afe97480e1
Guilherme Augusto <guilhermedev23@gmail.com>
2024-01-22 23:58:43 -0300

    tada: initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function togglemode() {
+  const html = document.documentElement
+
+  //if (body.classList.contains("light")) {
+  //  html.classList.remove("light")
+ // } else {
+    //html.classList.add("light")
+ // }
+ html.classList.toggle('light')
+}
